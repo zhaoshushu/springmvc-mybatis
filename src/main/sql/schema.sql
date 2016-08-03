2a5ab52944ce790abe9c7d7014bec0b98a1b7623
Administrator <Administrator@MICROSOFT>
2016-08-03 16:40:43 +0800

    z

---
+++ src/main/sql/schema.sql
@@ -0,0 +1,39 @@
+--数据库初始化脚本
+--创建数据库
+CREATE DATABASE seckill;
+--使用数据库
+use seckill;
+--创建秒杀库存表
+CREATE TABLE seckill(
+'seckill_id' bigint NOT NULL AUTO_INCREMENT COMMENT'商品库存id',
+'name' varchar NOT NULL COMMENT'商品名称',
+'number' int NOT NULL COMMENT'库存数量',
+'start_time' timestamp NOT NULL COMMENT'秒杀开启时间',
+'end_time' timestamp NOT NULL COMMENT'秒杀结束时间',
+'create_time' timestamp NOT NULL CURRENT_TIMESTAMP COMMENT'创建时间',
+PRIMARY KEY(seckill_id),
+key idx_start_time(start_time),
+key idx_end_time(end_time),
+key idx_create_time(create_time),
+)ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='秒杀库存表';
+
+--初始化数据
+insert into 
+    seckill(name,number,start_time,end_time)
+values
+	('1000元秒杀苹果',100,'2016-8-1 00:00:00','2016-8-2 00:00:00'),
+	('500元秒杀内裤',200,'2016-8-1 00:00:00','2016-8-2 00:00:00');	
+--秒杀成功明细表
+
+--用户登录认证相关的信息	
+create table success_killed(
+'seckill_id' bigint NOT NULL COMMENT'秒杀商品id',
+'user_phone' bigint NOT NULL COMMENT'用户手机号',
+'state' tinyint NOT NULL DEFAULT -1 COMMENT'状态标识：-1：无效  0：成功  1：已付款',
+'create_time' timestamp NOT NULL COMMENT'创建时间',
+PRIMARY KEY(seckill_id,user_phone),
+key idx_create_time(create_time)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='秒杀成功明细表';
+--连接数据库控制台
+mysql -uroot -p0102
+
